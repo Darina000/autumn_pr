147e5b7ad614f031e2ba997d233c55e40eadfefb
Darina000 <61585500+Darina000@users.noreply.github.com>
2020-09-23 23:23:25 +0300

    Update task_1.cpp

--- 3_week/home/task_1.cpp
+++ 3_week/home/task_1.cpp
@@ -5,59 +5,40 @@
 #include <iostream>
 #include <functional>
 
-
-
 //1
-void f1(const int& x){
+void f1(unsigned int k){
+   // double a = x;
     std::cout << "fun 1";
 };
-void f1(int x){
+void f1(double x){
     std::cout << "fun 2";
 };
 
 //2
-void f2(char x){
+void f2(const int& k, double m){
     std::cout << "fun 1";
 };
-void f2(char x, int y = 2){
+void f2(int y, double x){
     std::cout << "fun 2";
 };
 
 //3
-void f3(char x){
+void f3(int k = 3){
     std::cout << "fun 1";
 };
-void f3(unsigned char){
+void f3(double x = 6){
     std::cout << "fun 2";
 };
 
-//4
-void f4(float x){
-    std::cout << "fun 1";
-};
-void f4(double x){
-    std::cout << "fun 2";
-};
 
-//5
-void f5(int x, int y){
-    std::cout << "fun 1";
-};
-void f5(int x, int &y){
-    std::cout << "fun 2";
-};
+
 
 int main() {
-    int m = 3;
-    int g = 2;
-    double l = 2.3;
-    char k = 3;
     
-    //f1(m);
-    //f2(k);
-    //f3(70);
-    //f4(5);
-    //f5(m, g);
+    //f1(1);
+    //f2(3, 2);
+    //f3();
+    
     
     return 0;
 }

2e2cc0a3460f926e75f3144d4bc7d87c127b0d7c
Darina000 <61585500+Darina000@users.noreply.github.com>
2020-10-01 00:03:54 +0300

    Update task_1.cpp

--- 4_week/home/task_1.cpp
+++ 4_week/home/task_1.cpp
@@ -29,16 +29,6 @@ struct City{
         old.push_back(name);
     }
     
-    const double PI = 3.14159265;
-    
-    double f(const std::vector <double>& d1 ,const std::vector <double>& d2) const {
-        double R = 6371.0;
-        double lat = (d2[0]-d1[0])*(PI/180);
-        double lon = (d2[1]-d1[1])*(PI/180);
-        double rez = sin(lat/2)*sin(lat/2)+cos(d1[0]*(PI/180))*cos(d2[0]*(PI/180))*sin(lon/2)*sin(lon/2);
-        double d = 2*R*atan2(sqrt(rez), sqrt(1-rez));
-        return d;
-    }
     
     
     std::string name;
@@ -49,7 +39,15 @@ struct City{
 };
 
 void dist(const struct City* a, const struct City* b ){
-    std::cout << a->f(a->distantion, b->distantion) <<  std::endl;
+    std::vector <double> d1 = a->distantion;
+    std::vector <double> d2 = b->distantion;
+    const double PI = 3.14159265;
+    double R = 6371.0;
+    double lat = (d2[0]-d1[0])*(PI/180);
+    double lon = (d2[1]-d1[1])*(PI/180);
+    double rez = sin(lat/2)*sin(lat/2)+cos(d1[0]*(PI/180))*cos(d2[0]*(PI/180))*sin(lon/2)*sin(lon/2);
+    double d = 2*R*atan2(sqrt(rez), sqrt(1-rez));
+    std::cout << d <<  std::endl;
 }
 
 void Print_v(const std::vector <std::string>& v){

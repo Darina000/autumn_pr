22df375f7d1635c366b892a010845d4402f0886d
Darina000 <61585500+Darina000@users.noreply.github.com>
2020-09-30 22:10:48 +0300

    Update task_1.cpp

--- 4_week/home/task_1.cpp
+++ 4_week/home/task_1.cpp
@@ -1,7 +1,3 @@
-//  Created by Дарья Землянская on 26.09.2020.
-//  Copyright © 2020 Дарья Землянская. All rights reserved.
-//
-
 #include <iostream>
 #include <vector>
 #include <cmath>
@@ -9,35 +5,30 @@
 
 
 struct City{
-    City():name("Noname"), street({"None"}), distantion({0}) {
+    City():name{"Noname"}, street{"None"}, distantion{0}{
     }
     
     ~City(){
     }
     
-    City(std::string a, std::vector <std::string> s, std::vector <double> dist): name(a), street(s), distantion(dist)
+    City(const std::string &a,const  std::vector <std::string> &s, const std::vector <double> &dist): name(a), street(s), distantion(dist)
     {
         save_name(a);
     }
-
-    std::string name;
-    std::vector <std::string> street;
-    std::vector <double> distantion;
-    std::vector <std::string> old;
     
-    void change_name(std::string new_name){
+    void change_name(const std::string &new_name){
         name = new_name;
         save_name(new_name);
     }
     
-    void save_name(std::string name){
+    void save_name(const std::string& name){
         old.push_back(name);
     }
     
     const double PI = 3.14159265;
     
-    double Gaversinus(const std::vector <double>& d1 ,const std::vector <double>& d2) {
-        double R = 6371; // радиус Земли в км.
+    double f(const std::vector <double>& d1 ,const std::vector <double>& d2) const {
+        double R = 6371.0;
         double lat = (d2[0]-d1[0])*(PI/180);
         double lon = (d2[1]-d1[1])*(PI/180);
         double rez = sin(lat/2)*sin(lat/2)+cos(d1[0]*(PI/180))*cos(d2[0]*(PI/180))*sin(lon/2)*sin(lon/2);
@@ -45,11 +36,17 @@ struct City{
         return d;
     }
     
-    void dist(const struct City* a){
-        std::cout << Gaversinus(distantion, a->distantion) <<  std::endl;
-    }
+    
+    std::string name;
+    std::vector <std::string> street;
+    std::vector <double> distantion;
+    std::vector <std::string> old;
     
 };
+
+void dist(const struct City* a, const struct City* b ){
+    std::cout << a->f(a->distantion, b->distantion) <<  std::endl;
+}
 
 void Print_v(const std::vector <std::string>& v){
     for (auto i: v){
@@ -70,7 +67,7 @@ int main() {
     std::vector <std::string> v1 = {"Nevskiy", "Ligovskiy", "Sadovaya"};
     std::vector <double> d1 = {59.9, 30.34};
     
-    City Peter = {"St. Petersburg", v1, d1};
+    City Peter("St. Petersburg", v1, d1);
     City* Pt = &Peter;
     
     Pt -> change_name("Petrograd");
@@ -79,10 +76,10 @@ int main() {
     
     std::vector <std::string> v2 = {"Babaeva", "Arbat", "Tverskaya"};
     std::vector <double> d2 = {55.7, 37.6};
-    City Moscow = {"Moscow", v2, d2};
+    City Moscow("Moscow", v2, d2);
     City * Msc = &Moscow;
     
-    Msc -> dist(Pt);
+    dist(Pt, Msc);
 
     return 0;
 }

c199cdf986f4565945f6e8b92690215d46a8dc64
Darina000 <61585500+Darina000@users.noreply.github.com>
2020-10-05 22:02:43 +0300

    Add files via upload

---
+++ 5_week/home/task4/f2.cpp
@@ -0,0 +1,11 @@
+//  Created by Дарья Землянская on 05.10.2020.
+//  Copyright © 2020 Дарья Землянская. All rights reserved.
+//
+
+#include <stdio.h>
+#include "f.hpp"
+#include <iostream>
+
+//void f(){
+//    std::cout << "f2";
+//}

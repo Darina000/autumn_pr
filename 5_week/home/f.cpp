f934f4158f00806995b5df23f1d1569104d52fc5
Darina000 <61585500+Darina000@users.noreply.github.com>
2020-10-05 21:04:31 +0300

    Add files via upload

---
+++ 5_week/home/f.cpp
@@ -0,0 +1,10 @@
+//  Created by Дарья Землянская on 05.10.2020.
+//  Copyright © 2020 Дарья Землянская. All rights reserved.
+//
+
+#include "f.hpp"
+#include <iostream>
+
+//void f(){
+//    std::cout << "f" << std::endl;
+//}
---
+++ 5_week/home/f.hpp
@@ -0,0 +1,12 @@
+//  Created by Дарья Землянская on 05.10.2020.
+//  Copyright © 2020 Дарья Землянская. All rights reserved.
+//
+
+#ifndef f_hpp
+#define f_hpp
+
+#include <stdio.h>
+
+extern void f();
+
+#endif

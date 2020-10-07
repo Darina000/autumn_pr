c842e2687183858725925ebe7cc76bc69124079a
Darina000 <61585500+Darina000@users.noreply.github.com>
2020-10-08 00:18:23 +0300

    Update City.hpp

--- 5_week/home/task1/City.hpp
+++ 5_week/home/task1/City.hpp
@@ -4,30 +4,29 @@
 
 #ifndef City_hpp
 #define City_hpp
-
-#include <stdio.h>
+#include <vector>
+#include <iostream>
+#include <string>
 
 class City{
+public:
+    City():name{"Noname"}, street{"None"}, distantion{0}{}
+    ~City(){}
+    City(const std::string &a,const  std::vector <std::string> &s, const std::vector <double> &dist);
+    
+    const auto& return_dist()const { return distantion;};
+    const auto& return_old()const {return old;};
+    
+    
+    void change_name(const std::string &new_name);
+    void save_name(const std::string& name);
+    
+    friend std::ostream& operator<< (std::ostream &os, const City &city);
+    
 private:
     std::string name;
     std::vector <std::string> street;
     std::vector <double> distantion;
     std::vector <std::string> old;
     
-public:
-    City():name{"Noname"}, street{"None"}, distantion{0}{}
-    ~City(){}
-    City(const std::string &a,const  std::vector <std::string> &s, const std::vector <double> &dist);
-    
-    void change_name(const std::string &new_name);
-    void save_name(const std::string& name);
-    
-    std::vector <double> return_dist(){ return distantion;};
-    std::vector <std::string> return_old(){return old;};
-    
-    friend void dist( City& a,  City& b );
-    
 };
-
-
-#endif /* City_hpp */

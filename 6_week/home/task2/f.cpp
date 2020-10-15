b6a489d3a9bb6bb5e5530d04d506b9fd9ee429f6
Darina000 <61585500+Darina000@users.noreply.github.com>
2020-10-15 19:38:53 +0300

    Update f.cpp

--- 6_week/home/task2/f.cpp
+++ 6_week/home/task2/f.cpp
@@ -1,33 +1,37 @@
 //  Created by Дарья Землянская on 15.10.2020.
 //  Copyright © 2020 Дарья Землянская. All rights reserved.
 //
+#include <algorithm>
+#include <cmath>
 #include <iostream>
 #include <string>
-#include <cmath>
-#include <algorithm>
 
 #include "f.hpp"
 
 
 
-std::ostream& operator<< (std::ostream &os, const Frac &frac){
+std::ostream& operator<< (std::ostream &os, const Fraction &fraction){
+    
     os << "------" << std::endl;
     os << "Numerator: ";
-    os << frac.num << " ";
+    os << fraction.m_num << " ";
     os << std::endl;
+    
+    
     os << "Denominator : ";
-    os << frac.den  << std::endl;
+    os << fraction.m_den  << std::endl;
     os << "------" << std::endl;
     return os;
 };
 
-std::istream& operator>> (std::istream &in, Frac &frac)
+std::istream& operator>> (std::istream &in, Fraction &fraction)
 {
-    in >> frac.num;
-    in >> frac.den;
- 
+    char i;
+    in >> fraction.m_num >> i >> fraction.m_den;
+    fraction.reduce();
     return in;
 }
+
 
 int gcd(int a, int b) {
     if (a == b){
@@ -41,141 +45,125 @@ int gcd(int a, int b) {
     return a;
 }
 
+
 int lcm(int a, int b) {
     return gcd(a, b) * a * b;
 }
 
-void Frac::reduce() {
-    int gc = gcd(abs(num), den);
+
+void Fraction::reduce() {
+    int gc = gcd(abs(m_num), m_den);
     if (gc != 1) {
-        num = num / gc;
-        den = den/ gc;
+        m_num = m_num / gc;
+        m_den = m_den/ gc;
     }
 }
 
-
-Frac operator*(const Frac &frac1, const Frac &frac2) {
+Fraction operator*(const Fraction &frac1, const Fraction &frac2) {
     int num_res = frac1.getNumerator() * frac2.getNumerator();
     int den_res = frac1.getDenominator() * frac2.getDenominator();
-    Frac res(num_res, den_res);
-    res.reduce();
-    std::cout << "*" << std::endl;
-    std::cout << res << std::endl;
+    Fraction res(num_res, den_res);
     return res;
 }
 
-Frac operator/(const Frac &frac1, const Frac &frac2) {
+Fraction operator/(const Fraction &frac1, const Fraction &frac2) {
     int num_res = frac1.getNumerator() * frac2.getDenominator();
     int den_res = frac1.getDenominator() * frac2.getNumerator();
-    Frac res(num_res, den_res);
-    res.reduce();
-    std::cout << "/" << std::endl;
-    std::cout << res << std::endl;
+    Fraction res(num_res, den_res);
     return res;
 }
 
-Frac operator-(const Frac &frac1, const Frac &frac2) {
+Fraction operator-(const Fraction &frac1, const Fraction &frac2) {
     int num_res = frac1.getNumerator() * frac2.getDenominator() - frac1.getDenominator() * frac2.getNumerator();
     int den_res = lcm(frac1.getDenominator(), frac2.getDenominator());
-    Frac res(num_res, den_res);
-    res.reduce();
-    std::cout << "-" << std::endl;
-    std::cout << res << std::endl;
+    Fraction res(num_res, den_res);
     return res;
 }
 
-Frac operator+(const Frac &frac1, const Frac &frac2) {
+Fraction operator+(const Fraction &frac1, const Fraction &frac2) {
     int num_res = frac1.getNumerator() * frac2.getDenominator() + frac1.getDenominator() * frac2.getNumerator();
     int den_res = lcm(frac1.getDenominator(), frac2.getDenominator());
-    Frac res(num_res, den_res);
-    res.reduce();
-    std::cout << "+" << std::endl;
-    std::cout << res << std::endl;
+    Fraction res(num_res, den_res);
     return res;
 }
 
-bool operator== (const Frac &frac1, const Frac &frac2)
+bool operator== (const Fraction &frac1, const Fraction &frac2)
 {
-    return (frac1.getNumerator() * frac2.getDenominator() == frac1.getDenominator() * frac2.getNumerator());
+    return !(frac1 < frac2) && !(frac2 < frac1);
 }
-
-bool operator!= (const Frac &frac1, const Frac &frac2)
+bool operator!= (const Fraction &frac1, const Fraction &frac2)
 {
-    return !(frac1.getNumerator() * frac2.getDenominator() == frac1.getDenominator() * frac2.getNumerator());
+    return !(frac1 == frac1);
 }
-
-bool operator> (const Frac &frac1, const Frac &frac2)
+bool operator> (const Fraction &frac1, const Fraction &frac2)
 {
     return (frac1.getNumerator() * frac2.getDenominator() > frac1.getDenominator() * frac2.getNumerator());
 }
  
-bool operator>= (const Frac &frac1, const Frac &frac2)
+bool operator>= (const Fraction &frac1, const Fraction &frac2)
 {
     return (frac1.getNumerator() * frac2.getDenominator() >= frac1.getDenominator() * frac2.getNumerator());
 }
  
-bool operator< (const Frac &frac1, const Frac &frac2)
+bool operator< (const Fraction &frac1, const Fraction &frac2)
 {
-    return  (frac1.getNumerator() * frac2.getDenominator() < frac1.getDenominator() * frac2.getNumerator());
+    return  (frac1 > frac2);
 }
  
-bool operator<= (const Frac &frac1, const Frac &frac2)
+bool operator<= (const Fraction &frac1, const Fraction &frac2)
 {
-    return  (frac1.getNumerator() * frac2.getDenominator() <= frac1.getDenominator() * frac2.getNumerator());
+    return  (frac1 >= frac2);
 }
 
-Frac& Frac::operator++()
+Fraction& Fraction::operator++()
 {
-    num += den;
-    std::cout << "++" << std::endl;
-    std::cout << *this << std::endl;
+    m_num += m_den;
+    reduce();
     return *this;
+}
+Fraction Fraction::operator ++(int)
+{
+    Fraction temp(m_num);
+    ++(*this);
+    return temp;
 }
  
-Frac& Frac::operator--()
+Fraction& Fraction::operator--()
 {
-    num -= den;
-    std::cout << "--" << std::endl;
-    std::cout << *this << std::endl;
+    m_num -= m_den;
+    reduce();
     return *this;
 }
+Fraction Fraction::operator --(int)
+{
+    Fraction temp(m_num);
+    --(*this);
+    return temp;
+}
 
-Frac& Frac::operator+= (Frac const& frac) {
-    num = num * frac.getDenominator() + den * frac.getNumerator();
-    den = lcm((den), frac.getDenominator());
+Fraction& Fraction::operator+= (Fraction const& frac) {
+    m_num = m_num * frac.getDenominator() + m_den * frac.getNumerator();
+    m_den = lcm((m_den), frac.getDenominator());
     reduce();
-    std::cout << "+=" << std::endl;
-   std::cout << *this << std::endl;
   return *this;
 }
-Frac& Frac::operator-= (Frac const& frac) {
-    num = num * frac.getDenominator() - den * frac.getNumerator();
-    den = lcm((den), frac.getDenominator());
+Fraction& Fraction::operator-= (Fraction const& frac) {
+    m_num = m_num * frac.getDenominator() - m_den * frac.getNumerator();
+    m_den = lcm((m_den), frac.getDenominator());
     reduce();
-    std::cout << "-=" << std::endl;
-    std::cout << *this << std::endl;
   return *this;
 }
 
-Frac& Frac::operator*= (Frac const& frac) {
-    num = num * frac.getNumerator();
-    den = den * frac.getDenominator();
+Fraction& Fraction::operator*= (Fraction const& frac) {
+    m_num = m_num * frac.getNumerator();
+    m_den = m_den * frac.getDenominator();
     reduce();
-    std::cout << "*=" << std::endl;
-    std::cout << *this << std::endl;
     return *this;
 }
-Frac& Frac::operator/= (Frac const& frac) {
-    num = num * frac.getDenominator();
-    den = den * frac.getNumerator();
+Fraction& Fraction::operator/= (Fraction const& frac) {
+    m_num = m_num * frac.getDenominator();
+    m_den = m_den * frac.getNumerator();
     reduce();
-    std::cout << "/=" << std::endl;
-    std::cout << *this << std::endl;
     return *this;
 }
 
-double Frac::To_Float(){
-    double a = (num*1.0)/(den*1.0);
-    std::cout << "Float "  << a << std::endl;
-    return a;
-};

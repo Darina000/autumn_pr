2f5a2aa5f73f698d78d5fdd9c2357e08b0f70031
Darina000 <61585500+Darina000@users.noreply.github.com>
2020-10-23 11:13:26 +0300

    Update task1.cpp

--- 7_week/home/task1.cpp
+++ 7_week/home/task1.cpp
@@ -6,6 +6,7 @@
 #include <cstdint>
 #include <iostream>
 #include <utility>
+#include <vector>
  
 struct Point{
     double x;
@@ -14,34 +15,46 @@ struct Point{
 
 
 class Figure {
-    public:
-        static const unsigned int count = 4;
-        Point *array[count];
 public:
+        Figure(const std::vector<Point>& arrr): aray(arrr) {
+        }
+        ~Figure(){};
+    
         virtual double calc_square() const = 0;
         virtual double calc_perimeter() const = 0;
     
-        virtual void Print() const = 0;
-    friend std::ostream& operator<< (std::ostream &out, const Figure &figure);
+        friend std::ostream& operator<< (std::ostream &out, const Figure &figure);
+
+protected:
+        virtual void Print(std::ostream &out) const = 0;
+    
+    
+private:
+    std::vector <Point> aray;
 };
 
+
+
 std::ostream& operator<< (std::ostream &out, const Figure &figure){
-    figure.Print();
+    figure.Print(out);
+    out<< std::endl;
     return out;
 }
 
-
-double distance(Point pair1, Point pair2) {
-    double ans = sqrt(pow((pair1.x-pair2.x), 2)+pow((pair1.y-pair2.y), 2));
-    return ans;
+double distance(const Point &pair1, const Point &pair2) {
+    return sqrt(pow((pair1.x-pair2.x), 2)+pow((pair1.y-pair2.y), 2));
 }
 
+
 class Triangle: public Figure {
- 
     public:
-        double m_a, m_b, m_c;
- 
-    public:
+        Triangle(const std::vector<Point>& arrr):Figure(arrr){
+            m_a = distance(arrr[0], arrr[1]);
+            m_b = distance(arrr[1], arrr[2]);
+            m_c = distance(arrr[2], arrr[0]);
+        }
+        ~Triangle(){};
+    
         double calc_square() const {
             double m_p = (m_a + m_b + m_c)/2;
             return sqrt(m_p*(m_p - m_a) * (m_p - m_c) * (m_p - m_b));
@@ -50,31 +63,24 @@ class Triangle: public Figure {
         double calc_perimeter() const {
             return m_a + m_b + m_c;
         }
-    void Print() const{
-        std::cout << "Side: "<< m_a << " " << m_b << " " << m_c << std::endl;
-        std::cout << "Perimeter: " << calc_perimeter() << std::endl;
-        std::cout << "Square: " << calc_square() << std::endl;
-    }
-
-    Triangle(Point a1, Point a2, Point a3, Point a4 = {0, 0}){
-        
-                array[0] = &a1;
-                array[1] = &a2;
-                array[2] = &a3;
-        
-                m_a = distance(*array[0], *array[1]);
-                m_b = distance(*array[1], *array[2]);
-                m_c = distance(*array[2], *array[0]);
-            }
+    
+    protected:
+        void Print(std::ostream &out) const{
+            out << "Side: "<< m_a << " " << m_b << " " << m_c << std::endl;
+            out << "Perimeter: " << calc_perimeter() << std::endl;
+            out << "Square: " << calc_square() << std::endl;
+        }
+    private:
+        double m_a, m_b, m_c;
 };
  
 
 class Ellipce: public Figure {
- 
     public:
-        double a, b;
-    public:
- 
+        Ellipce(const Point O, double o_a, double o_b = 0, const std::vector<Point>& arrr = {}):Figure(arrr), center(O), a(o_a), b(o_b){
+        }
+        ~Ellipce(){};
+    
         double calc_square() const {
             return  M_PI * a * b;
         }
@@ -82,58 +88,59 @@ class Ellipce: public Figure {
         double calc_perimeter() const {
             return 4 * (M_PI * a*b +(a-b)*(a-b))/(a+b);
         }
-    
-        void Print() const{
-            std::cout << "Half-shafts: "<< a << " " << b << std::endl;
-            std::cout << "Perimeter: " << calc_perimeter() << std::endl;
-            std::cout << "Square: " << calc_square() << std::endl;
+    protected:
+        void Print(std::ostream &out) const{
+            out << "Center: "<< center.x << " " << center.y << std::endl;
+            out << "Half-shafts: "<< a << " " << b << std::endl;
+            out << "Perimeter: " << calc_perimeter() << std::endl;
+            out << "Square: " << calc_square() << std::endl;
         }
-    Ellipce(Point a1, Point a2, Point a3 = {0,0}, Point a4 = {0,0}){
-                array[0] = &a1;
-                array[1] = &a2;
-                array[2] = &a3;
-                array[3] = &a4;
-        
-                a = distance(*array[0], *array[2]);
-                b = distance(*array[1], *array[3]);
-    }
+    private:
+        const Point center;
+        double a, b;
 };
 
 
 
 class Circle: public Ellipce {
-private:
-    double r;
     public:
+        Circle(const Point O, double o_a): Ellipce(O, o_a), r(o_a), center(O){
+        }
+    
+        ~Circle(){};
+    
         double calc_perimeter() const {
             return 2 * M_PI * r;
         }
         double calc_square() const {
-            return  M_PI * a * b;
+            return  M_PI * r * r;
            }
-        void Print() const{
-            std::cout << "Radius: "<< r << std::endl;
-            std::cout << "Perimeter: " << calc_perimeter() << std::endl;
-            std::cout << "Square: " << calc_square() << std::endl;
+    protected:
+        void Print(std::ostream &out) const{
+            out << "Center: "<< center.x << " " << center.y << std::endl;
+            out << "Radius: "<< r << std::endl;
+            out << "Perimeter: " << calc_perimeter() << std::endl;
+            out << "Square: " << calc_square() << std::endl;
         }
-    Circle(Point a1, Point a2, Point a3 = {0,0}, Point a4 = {0,0}): Ellipce(a1, a2){
-       r = distance(*array[0], *array[1]);;
-    }
-        
+    private:
+        const Point center;
+        double r;
 };
 
 
 
 class Quadrangle: public Figure {
- 
     public:
- 
-        double a, b, c, d;
- 
-    public:
- 
+        Quadrangle(const std::vector<Point>& arrr):Figure(arrr){
+            a = distance(arrr[0], arrr[1]);
+            b = distance(arrr[1], arrr[2]);
+            c = distance(arrr[2], arrr[3]);
+            d = distance(arrr[3], arrr[0]);
+            m_q = distance(arrr[0], arrr[2]);
+        }
+        ~Quadrangle(){};
+    
         double calc_square() const {
-            double m_q = distance(*array[0], *array[2]);
             double m_p1 = a + b + m_q;
             double m_p2 = c + d + m_q;
             double s1 = sqrt(m_p1 * (m_p1 - a) * (m_p1 - b) * (m_p1 - m_q));
@@ -144,42 +151,45 @@ class Quadrangle: public Figure {
         double calc_perimeter() const {
             return a + b + c + d;
         }
-        void Print() const{
-            std::cout << "Side: "<< a << " " << b << " " << c << " " << d << std::endl;
-            std::cout << "Perimeter: " << calc_perimeter() << std::endl;
-            std::cout << "Square: " << calc_square() << std::endl;
+    protected:
+        void Print(std::ostream &out) const{
+            out << "Side: "<< a << " " << b << " " << c << " " << d << std::endl;
+            out << "Perimeter: " << calc_perimeter() << std::endl;
+            out << "Square: " << calc_square() << std::endl;
         }
-
-        Quadrangle(Point a1, Point a2, Point a3, Point a4){
-            array[0] = &a1;
-            array[1] = &a2;
-            array[2] = &a3;
-            array[3] = &a4;
-            
-            a = distance(*array[0], *array[1]);
-            b = distance(*array[1], *array[2]);
-            c = distance(*array[2], *array[3]);
-            d = distance(*array[3], *array[0]);
-        }
+    private:
+           double a, b, c, d, m_q;
 };
 
  
 class Rectangle: public Quadrangle{
  
     public:
+        Rectangle(const std::vector<Point>& arrr): Quadrangle(arrr){
+            a = distance(arrr[0], arrr[1]);
+            b = distance(arrr[1], arrr[2]);
+        }
+        ~Rectangle(){};
+    
         double calc_square() const {
             return a * b;
         }
-    Rectangle(Point a1, Point a2, Point a3 = {0,0}, Point a4 = {0,0}): Quadrangle(a1, a2, a3, a4){}
+    private:
+        double a, b;
 };
  
+
 class Square: public Rectangle{
     public:
- 
-        double calc_perimeter() const {
+        Square(const std::vector<Point>& arrr): Rectangle(arrr){
+            a = distance(arrr[0], arrr[1]);
+        }
+        ~Square(){};
+       double calc_perimeter() const {
             return 4 * a;
         }
-    Square(Point a1, Point a2 = {0,0}, Point a3 = {0,0}, Point a4 = {0,0}): Rectangle(a1, a2, a3, a4){}
+    private:
+        double a;
 };
  
 
@@ -190,20 +200,28 @@ int main() {
     Point t2{0, 0};
     Point t3{4, 0};
 
-    Point el_1{3, 0};
-    Point el_2{0, 2};
+    Point el_1{5, 1};
+    Point el_2{-4, 2};
     Point el_3{-3, 0};
     Point el_4{0, -2};
+    
+    std::vector<Point> triang ={t1, t2, t3};
+    std::vector<Point> quad ={el_1, el_2, el_3, el_4};
+    std::vector<Point> rec ={t1, t2, t3, el_1};
 
-    Triangle triangle(t1, t2, t3);
+    Triangle triangle(triang);
     std::cout << triangle;
-    Ellipce ellipce(el_1, el_2, el_3, el_4);
+    
+    Ellipce ellipce(t2, 5, 7);
     std::cout << ellipce;
-    Circle circle(el_1, el_2);
+    
+    Circle circle(t3, 5);
     std::cout << circle;
-    Quadrangle quadrangle(el_1, el_2, el_3, el_4);
+    
+    Quadrangle quadrangle(quad);
     std::cout << quadrangle;
-    Rectangle rectangle(el_1, el_2, el_3, el_4);
+    
+    Rectangle rectangle(rec);
     std::cout << rectangle;
     
     Figure *masssive[4] = { &triangle, &ellipce, &circle, &quadrangle};

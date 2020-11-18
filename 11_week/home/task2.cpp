01762df717ec3976c6be0d719d35416a18e2634d
Darina000 <61585500+Darina000@users.noreply.github.com>
2020-11-18 11:19:55 +0300

    Update task2.cpp

--- 11_week/home/task2.cpp
+++ 11_week/home/task2.cpp
@@ -8,17 +8,20 @@
 //делаем для разных типов очистку от константности
 template< typename T > struct remove_const{using type = T; };
 template< typename T > struct remove_const<const T>{ using type = T; };
-template< typename T > struct remove_const<const T&>{ using type = T; };
-template< typename T > struct remove_const<const T&&>{ using type = T; };
 //добавляем константность для любого принятного типа (очищая от остального)
 template< typename T > struct add_const{ using type = const T; };
 
+template<typename T>
+using remove_t = typename remove_const<T>::type;
+
+template<typename T>
+using add_t = typename add_const<T>::type;
+
+
 int main() {
     std::cout << std::boolalpha; //вкл отображение булевских значений в слова
-    typedef add_const<int>::type A; // добавим константность инту
-    std::cout << std::is_const<A>::value << std::endl; //проверим константность
-    typedef remove_const<A>::type B; //удалим константность и назовём тип
-    std::cout << std::is_const<B>::value << std::endl; // проверим отсутствие константности
+    std::cout << std::is_const <add_t<const int>>::value << std::endl; // добавим и проверим константность
+    std::cout << std::is_const <remove_t<const int>>::value << std::endl; // удалим константность b проверим отсутствие константности
     
   return 0;
 }

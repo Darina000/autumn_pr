590d384b95deda492426d7b547a5626240351212
Darina000 <61585500+Darina000@users.noreply.github.com>
2020-11-24 19:30:40 +0300

    Update task4.cpp

--- 12_week/home/task4.cpp
+++ 12_week/home/task4.cpp
@@ -2,31 +2,29 @@
 //  Copyright © 2020 Дарья Землянская. All rights reserved.
 //
 
-#include <iostream>
 #include <array>
+#include <iostream>
 
-template<int size>
-struct Simple{
-    std::array<int, size> arr;
-    constexpr Simple():arr(){
-        int value = 1;
-        for (int i = 0; i < size; i++)
-        {
-            bool prime = true;
-            do{
-                ++value;
-                for (int j = 0; j < i && arr[j] <= value / 2 && (prime = value % arr[j] != 0); ++j){};
-            } while (!prime);
-     arr[i] = value;
-        }
-    }
-    constexpr void print() const{
-        std::cout << arr[size-1] << std::endl;
-  }
-};
+template < int size >
+constexpr int Simple()
+{
+    std::array<int, size> arr{};
+    int value = 1;
+    for (int i = 0; i < size; i++)
+       {
+           bool prime = true;
+           do{
+               ++value;
+               for (int j = 0; j < i && arr[j] <= value / 2 && (prime = value % arr[j] != 0); ++j){};
+           } while (!prime);
+           arr[i] = value;
+       }
+    return arr[size-1];
+}
 
-int main() {
-  constexpr Simple arr = Simple<6>();
-  arr.print();
-  return 0;
+int main()
+{
+    constexpr double result = Simple<4>();
+    std::cout << result << std::endl;
+    return 0;
 }
